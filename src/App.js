7d8c0ee8a40c03ab44a13ecb004386140767a493
Jenny Zonka <jennycade@gmail.com>
2021-06-04 20:50:44 -0700

    Styled gameboards

--- src/App.js
+++ src/App.js
@@ -6,26 +6,43 @@ function App() {
     <div className="App">
       <header>BATTLESHIP</header>
       <div className="opponent gameboard">
+        <div className="blankspace"></div>
+
+        <div className="label xcoord">A</div>
+        <div className="label xcoord">B</div>
+        <div className="label xcoord">C</div>
+        <div className="label xcoord">D</div>
+        <div className="label xcoord">E</div>
+        <div className="label ycoord">1</div>
+
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">2</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">3</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">4</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">5</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
@@ -33,26 +50,43 @@ function App() {
         <div className="peg"> . </div>
       </div>
       <div className="player gameboard">
+        <div className="blankspace"></div>
+
+        <div className="label xcoord">A</div>
+        <div className="label xcoord">B</div>
+        <div className="label xcoord">C</div>
+        <div className="label xcoord">D</div>
+        <div className="label xcoord">E</div>
+        <div className="label ycoord">1</div>
+        
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">2</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">3</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">4</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
+
+        <div className="label ycoord">5</div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
         <div className="peg"> . </div>
